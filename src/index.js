bb9729daa5800ecfab74d55c13b71d850ced2681
DaemonGodx <pritam.2024ug@iiitranchi.ac.in>
2025-12-31 13:45:41 +0530

    fixed paths

--- src/index.js
+++ src/index.js
@@ -1,21 +1,40 @@
 import express from "express";
 import cookieParser from "cookie-parser";
-const app=express();
+import path from "path";
+import { fileURLToPath } from "url";
+
 import connect from "./connection/connection.js";
-import route from "./routes/index.js"
-import staticRoute from "./routes/staticRoute.js"
-import userRoute from "./routes/user.js"
+import route from "./routes/index.js";
+import staticRoute from "./routes/staticRoute.js";
+import userRoute from "./routes/user.js";
 import { MONGO_URL } from "./config/config.js";
-const url=MONGO_URL;
-import { checkAuth,restrictedTo  } from "./middlewares/auth.js";
-connect(url);
+import { checkAuth, restrictedTo } from "./middlewares/auth.js";
+
+const app = express();
+
+// ESM dirname fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+// DB
+connect(MONGO_URL);
+
+// Middlewares
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.set("view engine","ejs")
-app.set("views","./src/view")
-app.use(checkAuth)
-app.use("/",staticRoute)
-app.use("/user",userRoute)
-app.use("/url",restrictedTo(["Normal"]),route);
-app.listen(5000,()=>{console.log("Server is running on port 5000  ")})
+app.use(checkAuth);
+
+// View engine
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "view"));
+
+// Routes
+app.use("/", staticRoute);
+app.use("/user", userRoute);
+app.use("/url", restrictedTo(["Normal"]), route);
+
+// Server
+app.listen(5000, () => {
+  console.log("Server is running on port 5000");
+});
